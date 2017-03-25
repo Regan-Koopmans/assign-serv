8751c9d1f26ccebdc4eec6db8c779690f430f96d
Regan-Koopmans <regankoopmans@protonmail.com>
2017-03-25 16:10:25 +0200

    heroku push

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,22 @@
+
+/// Initial appointments load.
+
+$( document ).ready(function() {
+    $.ajax({url: "get-appointments", success: function(result){
+        
+    	$("#list").html("<br>You have the following appointments:");
+
+    	for (appointment of JSON.parse(result).appointments) {
+    		console.log(appointment);
+
+    		$("#list").append(appointmentCard(appointment));
+    	}
+    }});
+});
+
+
+// Subsequent appointment requests.
+
 $("#list-appointments").click(function(){
     $.ajax({url: "get-appointments", success: function(result){
         
@@ -15,6 +34,10 @@ $("#list-appointments").click(function(){
 
 function appointmentCard(appointment) {
 	result = "<div  class='card-panel container'>";
-	result += "<h4>" + appointment.title +"</h4></div>";
+	result += "<h5>" + appointment.title +"</h5>";
+	result += "<h6> Description: " + appointment.desc; + "</h6>";
+	result += "<h6> Date: " + appointment.date; + "</h6>";
+	result += "<h6> Time: " + appointment.time; + "</h6>";
+	result += "</div>";
 	return result; 
 }
